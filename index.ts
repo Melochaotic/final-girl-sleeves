6b497491497bde9fa6b33df5f43d8b42ea5a2426
Nicko J. Ruddock <NickoJRuddock@live.co.uk>
2025-03-02 22:30:05 +0000

    ✨ Basic script gets values from csv

--- index.ts
+++ index.ts
@@ -1,1 +1,42 @@
-console.log("It's working!!!");
+import * as fs from "fs";
+import * as path from "path";
+import { exit } from "process";
+
+const searchVal = "Panic at Station 2891";
+const fileName = path.resolve("data/FinalGirlSleeves.csv");
+
+fs.readFile(fileName, "utf8", (err, data) => {
+  if (err) console.error(err);
+
+  const rows = data
+    .split("\n")
+    .map((row) => row.split(",").map((cell) => cell.trim()));
+
+  const titles = rows.shift();
+  if (!titles) throw new Error("No titles found");
+
+  for (const [
+    year,
+    title,
+    sleeveType,
+    countStandard,
+    countEuro,
+    count70x121,
+    count65x130,
+  ] of rows) {
+    if (title === searchVal) {
+      console.log(
+        `The "${title}" box was released in ${year}.\nIt currently has ${sleeveType.toUpperCase()} sleeves.\n\n` +
+          `Type     | Card count\n` +
+          `---------|-------------------\n` +
+          `Standard | ${Number(countStandard) || "-"}\n` +
+          `Euro     | ${Number(countEuro) || "-"}\n` +
+          `70*121   | ${Number(count70x121) || "-"}\n` +
+          `65*130   | ${Number(count65x130) || "-"}`,
+      );
+      exit();
+    }
+  }
+
+  console.error("NO RECORD FOUND!");
+});
