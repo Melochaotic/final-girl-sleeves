94e913d0add1b3a67c9dd12d3edd1d6739ad8b77
Nicko J. Ruddock <NickoJRuddock@live.co.uk>
2025-03-04 21:40:47 +0000

    💄 Color type output

--- src/commands/detail.ts
+++ src/commands/detail.ts
@@ -1,6 +1,8 @@
 import * as fs from "fs";
 import * as path from "path";
 import { exit } from "process";
+import { styleText } from "util";
+import type { TableStructure } from "../types/TableStructure";
 
 export default function detail() {
   const searchTitle = process.argv[3];
@@ -26,10 +28,22 @@ export default function detail() {
       countEuro,
       count70x121,
       count65x130,
-    ] of rows) {
+    ] of rows as TableStructure[]) {
       if (title.toLowerCase() === searchTitle.toLowerCase()) {
+        const sleeveTypeColor =
+          sleeveType === "No" // Always wear protection
+            ? "red"
+            : sleeveType === "Ryker" // Aim to sleeve all w/ Ryker
+              ? "green"
+              : "yellow"; // Better than nothing
+
+        const sleeveTypeText = styleText(
+          [sleeveTypeColor, "bold"],
+          sleeveType.toUpperCase(),
+        );
+
         console.log(
-          `The "${title}" box was released in ${year}.\nIt currently has ${sleeveType.toUpperCase()} sleeves.\n\n` +
+          `The "${title}" box was released in ${year}.\nIt currently has ${sleeveTypeText} sleeves.\n\n` +
             `Type     | Count\n` +
             `---------|-------\n` +
             `Standard | ${Number(countStandard) || "-"}\n` +
