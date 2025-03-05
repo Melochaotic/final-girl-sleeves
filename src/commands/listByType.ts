22d77d16170b6936d998a3f69e0937e8dc9f8546
Nicko J. Ruddock <NickoJRuddock@live.co.uk>
2025-03-05 00:01:52 +0000

    ✨ List titles of a type

---
+++ src/commands/listByType.ts
@@ -0,0 +1,30 @@
+import * as fs from "fs";
+import * as path from "path";
+import { argv } from "process";
+import { SleeveType, TableStructure } from "../types/TableStructure";
+
+export default function listByType() {
+  const sleeveType = argv[3] as SleeveType;
+  const fileName = path.resolve("data/FinalGirlSleeves.csv");
+  fs.readFile(fileName, "utf8", (err, data) => {
+    if (err) throw err;
+
+    const rows = data
+      .split("\n")
+      .map((row) => row.split(",").map((cell) => cell.trim()));
+
+    const colTitles = rows.shift();
+    if (!colTitles) throw new Error("No column titles found");
+
+    const titleArr: string[] = [];
+    for (const row of rows as TableStructure[]) {
+      if (row[2].toLowerCase() === sleeveType.toLowerCase()) {
+        titleArr.push(row[1]); // title
+      }
+    }
+
+    console.log(`${titleArr.length} games with ${sleeveType} Sleeves`);
+    console.log(`-----------------------------`);
+    titleArr.sort().forEach((title) => console.log(`* ${title}`));
+  });
+}
