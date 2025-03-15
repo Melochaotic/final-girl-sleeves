580a4c45cfc40fc3acbaa6060571773676c4fc15
Nicko J. Ruddock <NickoJRuddock@live.co.uk>
2025-03-15 15:27:47 +0000

    ♻️ Merge `listByType` -> `list` command

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,15 +1,25 @@
+import { argv } from "process";
 import { styleText } from "util";
-import type { TableRow } from "../types/TableStructure";
+import type { SleeveType, TableRow } from "../types/TableStructure";
 import { parseCsv } from "../utils/csv.mts";
+import { formatSleeveType } from "../utils/formatting.mts";
 
 export default function () {
+  const sleeveType = argv[3] as SleeveType;
   const { rows } = parseCsv();
 
   const titleArr: string[] = [];
   for (const row of rows as TableRow[]) {
-    titleArr.push(row[1]); // title
+    if (!sleeveType || row[2].toLowerCase() === sleeveType.toLowerCase()) {
+      titleArr.push(row[1]); // title
+    }
   }
 
+  const sleeveTypeText = sleeveType
+    ? ` with ${formatSleeveType(sleeveType)} sleeeves`
+    : "";
+  console.log(`${titleArr.length} game boxes${sleeveTypeText}:`);
+  console.log(`-----------------------------`);
   titleArr
     .sort()
     .forEach((title) => console.log(`* ${styleText(["yellow"], title)}`));
