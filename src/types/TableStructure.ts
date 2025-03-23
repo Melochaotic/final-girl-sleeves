ce7647c462c0b5a13f247082e3903e36c73688c8
Nicko J. Ruddock <NickoJRuddock@live.co.uk>
2025-03-23 15:03:22 +0000

    🏷️ Add types for GameTitles

--- src/types/TableStructure.ts
+++ src/types/TableStructure.ts
@@ -1,12 +1,40 @@
-export const sleeveTypeArr = ["No", "Standard", "Premium", "Ryker"] as const;
-export type SleeveType = (typeof sleeveTypeArr)[number];
-
 export type TableRow = [
   string, // year
-  string, // title
+  GameTitle, // title
   SleeveType, // sleeveType
   string, // countStandard
   string, // countEuro
   string, // count70x121
   string, // count65x130
 ];
+
+export const sleeveTypeArr = ["No", "Standard", "Premium", "Ryker"] as const;
+export type SleeveType = (typeof sleeveTypeArr)[number];
+
+export const gameTitleArr = [
+  "A Knock at the Door",
+  "Accessories Box",
+  "Box of Props",
+  "Carnage at the Carnival",
+  "Core",
+  "Don't Make a Sound",
+  "Frightmare on Maple Lane",
+  "Guest Stars",
+  "Hell to Pay",
+  "Into the Void",
+  "Madness in the Dark",
+  "Once Upon a Full Moon",
+  "Panic at Station 2891",
+  "Series 3 Bonus Features",
+  "Slaughter in the Groves",
+  "Terror from Above",
+  "Terror from Destiny",
+  "Terror from the Grave",
+  "The Falconwood Files",
+  "The Happy Trails Horror",
+  "The Haunting of Creech Manor",
+  "The Killer from Tomorrow",
+  "The Marrek Murders",
+  "The North Pole Nightmare",
+] as const;
+export type GameTitle = (typeof gameTitleArr)[number];
