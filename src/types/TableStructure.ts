18cb606376401d62441c6a94040160d419f49ca5
Nicko J. Ruddock <NickoJRuddock@live.co.uk>
2026-07-10 15:40:32 +0100

    🗃️ Add new FG titles

--- src/types/TableStructure.ts
+++ src/types/TableStructure.ts
@@ -12,8 +12,11 @@ export const sleeveTypeArr = ["No", "Standard", "Premium", "Ryker"] as const;
 export type SleeveType = (typeof sleeveTypeArr)[number];
 
 export const gameTitleArr = [
+  "A Demon in the Shadows",
   "A Knock at the Door",
+  "A Rotten Harvest",
   "Accessories Box",
+  "Bad Times at Buddyland",
   "Box of Props",
   "Carnage at the Carnival",
   "Core",
@@ -26,15 +29,18 @@ export const gameTitleArr = [
   "Once Upon a Full Moon",
   "Panic at Station 2891",
   "Series 3 Bonus Features",
+  "Shriek",
   "Slaughter in the Groves",
   "Terror from Above",
   "Terror from Destiny",
   "Terror from the Grave",
   "The Falconwood Files",
+  "The Green Terror",
   "The Happy Trails Horror",
   "The Haunting of Creech Manor",
   "The Killer from Tomorrow",
   "The Marrek Murders",
   "The North Pole Nightmare",
+  "The Raptor Games",
 ] as const;
 export type GameTitle = (typeof gameTitleArr)[number];
